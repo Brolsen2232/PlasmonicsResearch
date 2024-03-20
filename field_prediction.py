25541c778e1639d0193892038ca63a6ae4916858
brolsen <brolsen2232@gmail.com>
2024-03-20 10:01:40 -0700

    FNOBlock architecture changes

--- field_prediction.py
+++ field_prediction.py
@@ -1,40 +1,54 @@
 import torch.nn as nn
 import torch
-import torch.nn as nn
-import torch
+
+
 
 class FieldPredictionNetwork(nn.Module):
     def __init__(self, geometry_dim, field_dim, hidden_layers):
         super().__init__()
-        self.output_linear = nn.Linear(geometry_dim, geometry_dim, dtype=torch.complex64) 
-        self.fourier_blocks = nn.ModuleList()
-        for _ in range(2):  
-            self.fourier_blocks.append(FourierBlock())
-        self.intermediate_layer = nn.Linear(geometry_dim, 32, dtype=torch.complex64) 
-        self.output_layer = nn.Linear(hidden_layers[-1], field_dim, dtype=torch.complex64)  
+
+        self.input_layer = nn.Linear(geometry_dim, hidden_layers[0])  
+        self.interm_layers = nn.ModuleList([
+            nn.Linear(hidden_layers[i], hidden_layers[i+1]) 
+            for i in range(len(hidden_layers) - 1)
+        ])
+        self.fno_block = FNOBlock(modes=32, width=64) 
+        self.output_layer = nn.Linear(hidden_layers[-1], field_dim) 
 
     def forward(self, geometry_input):
-        geometry_input = geometry_input.unsqueeze(0)  
+        x = self.input_layer(geometry_input) 
+        x = torch.relu(x)  
 
-        x = self.output_linear(geometry_input) 
-        
-        for block in self.fourier_blocks:
-            x = block(x)
-        x = self.intermediate_layer(x)
+        for layer in self.interm_layers:
+            x = torch.relu(layer(x))
+
+        x = self.fno_block(x)
+
         field_prediction = self.output_layer(x)
         return field_prediction
 
 
-class FourierBlock(nn.Module):
-    def __init__(self):
+class FNOBlock(nn.Module):
+    def __init__(self, modes, width):
         super().__init__()
-        self.linear = nn.Linear(7,7)
-        self.learned_kernel = nn.Parameter(torch.randn(3, 3, 3, 1))  
- 
+        self.modes = modes
+        self.width = width
+        self.weights = nn.ParameterList([nn.Parameter(torch.randn(modes, width)) for _ in range(2)]) 
+        self.lin1 = nn.Linear(width, width)
+        self.lin2 = nn.Linear(width, width)
+
 
     def forward(self, x):
-        x_ft = torch.fft.fftn(x)
-        x_ft = x_ft * self.learned_kernel 
-        x = torch.fft.ifftn(x_ft)
-        return x 
+        
+        x = torch.fft.fftn(x)
+        L = torch.diag(torch.exp(1j * 2 * np.pi * torch.arange(self.modes) / self.modes)) 
 
+        x = x.unsqueeze(-1) @ self.weights[0] 
+        x = torch.relu(x)
+        x = L @ x  
+        x = x @ self.weights[1]
+        x = self.lin1(x.squeeze(-1))
+        x = torch.relu(x)
+        x = self.lin2(x)
+        x = torch.fft.ifftn(x)
+        return x
